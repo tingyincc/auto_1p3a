05a9744184166bfa73a3528c768278e9f2c28a1d
tingyincc <tingyincc@gmail.com>
2020-07-20 17:35:17 +0800

    0711:url and captcha changed to gif. fix bug for these updates

--- auto_1p3a.py
+++ auto_1p3a.py
@@ -3,7 +3,8 @@ import re
 import json
 import shutil
 import os
-
+from io import BytesIO
+from PIL import Image
 from predict import image_test_color_seg
 
 # for collecting images
@@ -19,6 +20,7 @@ class AutoPunch:
         self.login_url = 'https://www.1point3acres.com/bbs/member.php?mod=logging&action=login&loginsubmit=yes&infloat=yes&lssubmit=yes&inajax=1'
         self.punch_url = 'https://www.1point3acres.com/bbs/plugin.php?id=dsu_paulsign:sign&operation=qiandao&infloat=1&sign_as=1&inajax=1'
         self.verify_url = 'https://www.1point3acres.com/bbs/misc.php?mod=seccode&action=update&idhash=SA00&inajax=1&ajaxtarget=seccode_SA00'
+        self.verify_url_check = 'https://www.1point3acres.com/bbs/misc.php?mod=seccode&action=update&idhash=S00&inajax=1&ajaxtarget=seccode_S00'  # 0711 new
         self.session = requests.Session()
         self.headers = {
             'origin': 'https://www.1point3acres.com',
@@ -69,7 +71,7 @@ class AutoPunch:
                                         'qdmode': 2,
                                         'todaysay': 'check in',
                                         'fastreply': 0,
-                                        'sechash': 'SA00',
+                                        'sechash': 'S00',
                                         'seccodeverify': code
                                     })
         return res
@@ -131,25 +133,45 @@ class AutoPunch:
 
         return index+1
 
-    def crack_verify(self):
-        res = self.session.get(
-            self.verify_url, stream=True, headers=self.headers)
+    def crack_verify(self, option):
+
+        if(option == 1):
+            res = self.session.get(
+                self.verify_url_check, stream=True, headers=self.headers)
+        else:
+            res = self.session.get(
+                self.verify_url, stream=True, headers=self.headers)
         try:
             result = re.search('src="(.*)" class="vm"', res.text)
         except:
             return None
         res = self.session.get("https://www.1point3acres.com/bbs/" +
                                result.group(1), stream=True, headers=self.headers)
-        with open("img.jpg", 'wb') as fp:
-            fp.write(res.content)
-        captcha_text = image_test_color_seg("img.jpg")
+        # with open("img.jpg", 'wb') as fp:
+        #    fp.write(res.content)
+
+        # Credict:Hanyang-Li/BotFarmer-1point3acres
+        verify_gif = Image.open(BytesIO(res.content))
+        durations = []
+        try:
+            while True:
+                durations.append(verify_gif.info['duration'])
+                verify_gif.seek(verify_gif.tell() + 1)
+        except EOFError:
+            pass
+        verify_gif.seek(durations.index(max(durations)))
+        verify_image = Image.new('RGB', verify_gif.size)
+        verify_image.paste(verify_gif)
+        verify_image.save(str(option)+"_img.png")
+
+        captcha_text = image_test_color_seg(str(option)+"_img.png")
         print("Result of Cracking: ", captcha_text)
         return captcha_text
 
     def answer(self, formhash, code, answer):
         res = self.session.post(self.question_url,
                                 headers=self.headers,
-                                #cookies= cookieJar,
+                                # cookies= cookieJar,
                                 data={
                                     'formhash': formhash,
                                     'answer': answer,
@@ -181,7 +203,7 @@ if __name__ == '__main__':
         login_formhash = ap.get_punch_formhash()
         if(login_formhash):
             # need code from May 30, maybe server detected auto check in
-            code = ap.crack_verify()
+            code = ap.crack_verify(1)
             ap.punch(login_formhash, code)
         else:
             print(account['id'] + ' already checked in.')
@@ -197,7 +219,7 @@ if __name__ == '__main__':
             print("No question")
 
         if(formhash and question and answer_index):
-            code = ap.crack_verify()
+            code = ap.crack_verify(2)
             ap.answer(formhash, code, answer_index)
         else:
             print(account['id'] + ' already answered')
--- utils.py
+++ utils.py
@@ -7,6 +7,24 @@ import cv2
 
 def segmentDigit_binary(image_path):
     image = cv2.imread(image_path)
+
+    Z = image.reshape((-1, 3))
+
+    # convert to np.float32
+    Z = np.float32(Z)
+
+    # define criteria, number of clusters(K) and apply kmeans()
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+    K = 18
+    ret, label, center = cv2.kmeans(
+        Z, K, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
+
+    # Now convert back into uint8, and make original image
+    center = np.uint8(center)
+    res = center[label.flatten()]
+    image = res.reshape((image.shape))
+    cv2.imwrite("kmeans.png", image)
+
     h = image.shape[0]
     w = image.shape[1]
 
@@ -31,20 +49,29 @@ def segmentDigit_binary(image_path):
         image_stack.append(singlecolorimage)
 
     rectList = []
+    # t = 0
     for im in image_stack:
         gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
         ret, binary = cv2.threshold(gray, 1, 255, cv2.THRESH_BINARY)
         binary2, contours, hierarchy = cv2.findContours(
             binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
         for i in range(0, len(contours)):
-            if(len(contours) > 1):
-                break
+            # if(len(contours) > 5): break
             x, y, w, h = cv2.boundingRect(contours[i])
-            if(600 > w*h > 150 and w < 30 and h < 30 and np.count_nonzero(im)/(w*h) > 0.5):
+            # cv2.imwrite(str(t)+"_con.png", cv2.resize(
+            #     image[y:y+h, x:x+w], (20, 20), interpolation=cv2.INTER_AREA))
+            # t += 1
+            if(600 > w*h > 150 and w < 25 and h < 30 and np.count_nonzero(im)/(w*h) > 0.5):
                 rectList.append([x, y, w, h, np.count_nonzero(im)/(w*h)])
                 final_binary = cv2.bitwise_or(final_binary, binary)
 
     rectList = sorted(rectList, key=lambda l: l[0])
+
+    # for i, r in enumerate(rectList):
+    #     x, y, w, h = r[0], r[1], r[2], r[3]
+    #     cv2.imwrite(str(i)+".png", cv2.resize(
+    #         final_binary[y:y+h, x:x+w], (20, 20), interpolation=cv2.INTER_AREA))
+
     while(len(rectList) > 4):
         rectList = getOutlierIndex(rectList)
 
